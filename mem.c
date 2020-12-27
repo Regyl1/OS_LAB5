3dc11ec1cad2a4b03f6260946156a7b90009b7d6
Regyl1 <roma.orsha@mail.ru>
2020-12-27 11:37:14 +0300

    firstStep

---
+++ mem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <time.h>
+int main()
+{
+	char* a;
+	long long int size = 0, i = 0;
+	FILE* file1 = fopen("report.log", "w");
+	fclose(file1);
+	while(1){
+		size += 1;
+		a = malloc(1);
+		i++;
+		if(i % 100000 == 0){
+			FILE* file = fopen("report.log", "a");
+			fprintf(file, "%d\n", size);
+			fclose(file);
+		}
+	}
+return 0;
+}
